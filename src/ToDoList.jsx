95b497b7f10afb063431b9515a4892e951c72702
ezechias1 <137159815+ezechias1@users.noreply.github.com>
2024-06-12 16:02:05 +0200

    added functions

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -9,14 +9,36 @@ function ToDoList() {
   }
 
   function addTask() {
+
+
+  if(newTask.trim() !==""){
     setNewTask([...t, newTask])
-  setNewTask("")}
+  setNewTask("")}}
 
-  function deleteTask(index) {}
+  function deleteTask(index) {
+    const updatedtasks= tasks.filter((_, i) =>i !==index)
+    setNewTask(updatedtasks); 
+  }
 
-  function moveTaskUp(index) {}
+  function moveTaskUp(index) {
+if(index > 0){
+  const updatedtasks= [...tasks];
+  [updatedtasks[index], updatedtasks[index-1]]=
+  [updatedtasks[index-1], updatedtasks[index]];
+  setTasks(updatedtasks)
+}
 
-  function moveTaskDown(index) {}
+  }
+
+  function moveTaskDown(index) {
+
+    if(index < tasks.length -1){
+      const updatedtasks= [...tasks];
+      [updatedtasks[index], updatedtasks[index + 1]]=
+      [updatedtasks[index + 1], updatedtasks[index]];
+      setTasks(updatedtasks)
+    }
+  }
 
   return (
     <div className="to-do-list">
